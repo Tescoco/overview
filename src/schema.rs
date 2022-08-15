50a1729cd3385fa7d9c59b9f0c723f0c0b89fd64
Teslim Akinremi <ajokeakinremi43@gmail.com>
2022-08-15 10:43:53 +0100

    client and api completed

---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+table! {
+    overview (id) {
+        id -> Int4,
+        lat -> Nullable<Text>,
+        lon -> Nullable<Text>,
+        title -> Nullable<Text>,
+        context -> Nullable<Text>,
+        created -> Nullable<Timestamp>,
+    }
+}
